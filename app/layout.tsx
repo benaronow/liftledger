640f934bec8e00d1b9722f639167e45e18095b9b
benaronow <benaronow227@gmail.com>
2024-12-25 05:22:46 -0600

    ?

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,6 @@ export default function RootLayout({ children }: Props) {
       <html lang="en">
         <head>
           <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
             rel="icon"
             type="image/png"
             sizes="32x32"
@@ -31,7 +26,6 @@ export default function RootLayout({ children }: Props) {
             href="/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
-          <meta name="apple-mobile-web-app-title" content="LiftLedger" />
         </head>
         <body>
           <section className={styles.container}>
