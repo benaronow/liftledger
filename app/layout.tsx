4033ccb4166e82a97e99d33b10f2a1ddb55b6c7a
benaronow <benaronow227@gmail.com>
2024-12-25 06:07:13 -0600

    more

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,26 +13,31 @@ export default function RootLayout({ children }: Props) {
     <StoreProvider>
       <html lang="en">
         <head>
+          <meta
+            name="description"
+            content="An app for tracking resistance-training progression."
+          ></meta>
+          <meta name="apple-mobile-web-app-title" content="LiftLedger" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <link rel="apple-touch-icon" sizes="96x96" href="icon/icon-96.png" />
           <link
             rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
+            sizes="196x196"
+            href="icon/icon-196.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="icon/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="icon/favicon-16x16.png"
           />
-          <link rel="manifest" href="/site.webmanifest" />
-          <meta name="apple-mobile-web-app-title" content="LiftLedger" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <link rel="manifest" href="manifest.json" />
         </head>
         <body>
           <section className={styles.container}>
