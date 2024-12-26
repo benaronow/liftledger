a04db6ae87bed8b77831d514801fa373f271c51b
benaronow <benaronow227@gmail.com>
2024-12-25 21:34:43 -0600

    17

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -13,14 +13,15 @@ export const Header = () => {
   });
 
   type LLTitleProps = {
+    fontSize: string;
     fontFamily: string;
     fontWeight: string;
   };
 
   const LLTitle = styled("span")<LLTitleProps>(
-    ({ fontFamily, fontWeight }) => ({
+    ({ fontSize, fontFamily, fontWeight }) => ({
       color: "white",
-      fontSize: "30px",
+      fontSize: fontSize,
       fontFamily: fontFamily,
       fontWeight: fontWeight,
     })
@@ -29,6 +30,45 @@ export const Header = () => {
   return (
     <>
       <LLHeader>
+        <LLTitle fontSize="25px" fontFamily="Mina" fontWeight="700">
+          LiftLedger
+        </LLTitle>
+        {/* <div style={{ display: "flex" }}>
+          <div style={{ transform: "translate(0, -9px)" }}>
+            <LLTitle fontSize="55px" fontFamily="Iceberg" fontWeight="700">
+              L
+            </LLTitle>
+          </div>
+          <LLTitle fontSize="40px" fontFamily="Mina" fontWeight="700">
+            ift
+          </LLTitle>
+          <div style={{ transform: "translate(0, -9px)" }}>
+            <LLTitle fontSize="55px" fontFamily="Iceberg" fontWeight="700">
+              L
+            </LLTitle>
+          </div>
+          <LLTitle fontSize="40px" fontFamily="Mina" fontWeight="700">
+            edger
+          </LLTitle>
+        </div> */}
+        {/* <div style={{ display: "flex" }}>
+          <div style={{ transform: "translate(0, -17px)" }}>
+            <LLTitle fontSize="55px" fontFamily="Jockey One" fontWeight="700">
+              L
+            </LLTitle>
+          </div>
+          <LLTitle fontSize="40px" fontFamily="Mina" fontWeight="700">
+            ift
+          </LLTitle>
+          <div style={{ transform: "translate(0, -17px)" }}>
+            <LLTitle fontSize="55px" fontFamily="Jockey One" fontWeight="700">
+              L
+            </LLTitle>
+          </div>
+          <LLTitle fontSize="40px" fontFamily="Mina" fontWeight="700">
+            edger
+          </LLTitle>
+        </div> */}
         {/* <LLTitle fontFamily="Audiowide" fontWeight="400">
           1. LiftLedger
         </LLTitle>
@@ -44,9 +84,9 @@ export const Header = () => {
         {/* <LLTitle fontFamily="Fugaz One" fontWeight="400">
           5. LiftLedger
         </LLTitle> */}
-        <LLTitle fontFamily="Racing Sans One" fontWeight="400">
+        {/* <LLTitle fontFamily="Racing Sans One" fontWeight="400">
           6. LiftLedger
-        </LLTitle>
+        </LLTitle> */}
         {/* <LLTitle fontFamily="Edu AU VIC WA NT Guides" fontWeight="700">
           7. LiftLedger
         </LLTitle> */}
@@ -59,33 +99,33 @@ export const Header = () => {
         {/* <LLTitle fontFamily="Contrail One" fontWeight="400">
           10. LiftLedger
         </LLTitle> */}
-        <LLTitle fontFamily="Zen Dots" fontWeight="400">
+        {/* <LLTitle fontFamily="Zen Dots" fontWeight="400">
           11. LiftLedger
-        </LLTitle>
+        </LLTitle> */}
         {/* <LLTitle fontFamily="Iceberg" fontWeight="400">
           12. LiftLedger
         </LLTitle> */}
         {/* <LLTitle fontFamily="Jockey One" fontWeight="400">
           13. LiftLedger
         </LLTitle> */}
-        <LLTitle fontFamily="Goldman" fontWeight="700">
-          14. LiftLedger
-        </LLTitle>
+        {/* <LLTitle fontFamily="Goldman" fontWeight="700">
+          LiftLedger
+        </LLTitle> */}
         {/* <LLTitle fontFamily="Space Mono" fontWeight="700">
           15. LiftLedger
         </LLTitle> */}
         {/* <LLTitle fontFamily="ZCOOL QingKe HuangYou" fontWeight="400">
           16. LiftLedger
         </LLTitle> */}
-        <LLTitle fontFamily="Mina" fontWeight="700">
-          17. LiftLedger
-        </LLTitle>
+        {/* <LLTitle fontFamily="Mina" fontWeight="700">
+          LiftLedger
+        </LLTitle> */}
         {/* <LLTitle fontFamily="Odibee Sans" fontWeight="400">
           18. LiftLedger
         </LLTitle> */}
-        <LLTitle fontFamily="Baumans" fontWeight="400">
+        {/* <LLTitle fontFamily="Baumans" fontWeight="400">
           19. LiftLedger
-        </LLTitle>
+        </LLTitle> */}
         {/* <LLTitle fontFamily="Keania One" fontWeight="400">
           20. LiftLedger
         </LLTitle> */}
