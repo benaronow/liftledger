f8c2f9c13988747dfd6aeed77bb5c148bf5e4d49
benaronow <benaronow227@gmail.com>
2024-12-29 07:27:43 -0600

    app can build

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import type { Metadata } from "next";
-import { auth0 } from "@/lib/auth0";
 import { LiftLedger } from "./components/liftLedger";
 
-const IndexPage = async () => {
-  const session = await auth0.getSession();
-  return (
-    <LiftLedger session={session}/>
-  );
-}
+const IndexPage = () => {
+  return <LiftLedger />;
+};
 
 export const metadata: Metadata = {
   title: "LiftLedger",
