9da05edd5397035ffadf42db520be648fb1891b5
benaronow <benaronow227@gmail.com>
2025-02-26 02:05:55 -0500

    theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ const RootLayout = async ({ children }: Props) => {
           ></meta>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="theme-color" content="#a3248d" />
+          <meta name="theme-color" content="#6d6e71" />
           <meta httpEquiv="ScreenOrientation" content="autoRotate:disabled" />
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <link
