28a476b369b860888b63a9e7464acb1e52e89e9e
Ben Aronow <benaronow227@MacBook-Pro.local>
2025-05-20 23:24:51 -0500

    dashed lines

--- app/components/completeDay/SetChips.tsx
+++ app/components/completeDay/SetChips.tsx
@@ -38,11 +38,11 @@ const useStyles = makeStyles()({
   },
   nextChip: {
     background: "#004b7f",
-    border: "dotted 2px #0096FF",
+    border: "dashed 2px #0096FF",
   },
   incompleteChip: {
     background: "#58585b",
-    border: "dotted 2px #0096FF",
+    border: "dashed 2px #0096FF",
   },
   emptyChip: {
     background: "transparent",
