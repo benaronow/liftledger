74289f8ae24e605123a6056facf4f406f43bf29a
Ben Aronow <benaronow227@MacBook-Pro.local>
2025-10-19 20:06:38 -0500

    copilot actually helped for once

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,6 +78,11 @@ const RootLayout = async ({ children }: Props) => {
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
           crossOrigin="anonymous"
         />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `html, body { background-color: #131314 !important; }`,
+          }}
+        />
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
