dad00c8cb09f2b9ef6621ceb3b65e80c91a44170
Ben Aronow <benaronow227@MacBook-Pro.local>
2025-10-20 22:20:38 -0500

    dashboard bootstrap

--- app/dashboard/Dashboard.tsx
+++ app/dashboard/Dashboard.tsx
@@ -6,14 +6,12 @@ import { useUser } from "../providers/UserProvider";
 import dayjs from "dayjs";
 import { Day, Exercise, RouteType, Set, WeightType } from "@/lib/types";
 import { Spinner } from "../components/spinner";
-import { useDashboardStyles } from "./useDashboardStyles";
 import Link from "next/link";
 import { useScreenState } from "@/app/providers/ScreenStateProvider";
 import { checkIsBlockDone } from "@/app/utils";
 import { useBlock } from "@/app/providers/BlockProvider";
 
 export const Dashboard = () => {
-  const { classes } = useDashboardStyles();
   const { session, attemptedLogin, curUser } = useUser();
   const { curBlock, curBlockLoading } = useBlock();
   const { isFetching, toggleScreenState } = useScreenState();
@@ -120,40 +118,94 @@ export const Dashboard = () => {
     { metric: "Total Weight Lifted:", value: getTotalWeight("lbs") },
   ];
 
-  if ((session && !curUser) || curBlockLoading || isFetching) return <Spinner />;
+  if ((session && !curUser) || curBlockLoading || isFetching)
+    return <Spinner />;
 
   return (
-    <div className={classes.container}>
+    <div
+      className="d-flex flex-column align-items-center justify-content-evenly w-100"
+      style={{
+        position: "relative",
+        height: "100dvh",
+        padding: "50px 50px 70px",
+      }}
+    >
       {session ? (
         <>
-          <div className={classes.titleContainer}>
+          <div
+            className="d-flex flex-column align-items-center text-white"
+            style={{ fontFamily: "League+Spartan", fontWeight: 900 }}
+          >
             {curUser && (!curBlock || checkIsBlockDone(curBlock)) ? (
-              <span className={classes.titleSmall}>
+              <span
+                className="text-nowrap"
+                style={{ fontSize: "16px", marginBottom: "5px" }}
+              >
                 Create a training block to get started!
               </span>
             ) : (
               <>
-                <span className={classes.titleSmall}>
+                <span
+                  className="text-nowrap"
+                  style={{ fontSize: "16px", marginBottom: "5px" }}
+                >
                   Currently Completing:
                 </span>
-                <span className={classes.titleBig}>{curBlock?.name}</span>
+                <span style={{ fontSize: "24px" }}>{curBlock?.name}</span>
               </>
             )}
           </div>
           {curBlock && !checkIsBlockDone(curBlock) && (
             <>
               {metricValueMap.map((pair, idx) => (
-                <div key={idx} className={classes.entry}>
-                  <span className={classes.name}>{pair.metric}</span>
-                  <span className={classes.value}>{pair.value}</span>
+                <div
+                  key={idx}
+                  className="d-flex align-items-center justify-content-around w-100 text-white text-nowrap"
+                  style={{
+                    fontFamily: "League+Spartan",
+                    fontSize: "16px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  <span
+                    className="d-flex fw-bold justify-content-start text-start"
+                    style={{ width: "75%" }}
+                  >
+                    {pair.metric}
+                  </span>
+                  <span
+                    className="d-flex justify-content-end text-end"
+                    style={{ width: "125%" }}
+                  >
+                    {pair.value}
+                  </span>
                 </div>
               ))}
-              <div className={classes.buttonContainer}>
+              <div className="w-100" style={{ height: "67px" }}>
                 <div
-                  className={`${classes.startButtonBase} ${classes.startButtonBottom}`}
-                ></div>
+                  className="d-flex border border-0 justify-content-center align-items-center text-white text-decoration-none"
+                  style={{
+                    fontFamily: "League+Spartan",
+                    fontSize: "20px",
+                    fontWeight: 600,
+                    borderRadius: "25px",
+                    height: "60px",
+                    background: "#004c81",
+                    transform: "translateY(7px)",
+                  }}
+                />
                 <Link
-                  className={`${classes.startButtonBase} ${classes.startButtonTop}`}
+                  className="d-flex w-100 border border-0 justify-content-center align-items-center text-white text-decoration-none"
+                  style={{
+                    fontFamily: "League+Spartan",
+                    fontSize: "20px",
+                    fontWeight: 600,
+                    borderRadius: "25px",
+                    height: "60px",
+                    background: "#0096FF",
+                    transition: "transform 0.1s",
+                    transform: "translateY(-60px)",
+                  }}
                   href={RouteType.Workout}
                   onClick={() => {
                     toggleScreenState("fetching", true);
@@ -166,16 +218,48 @@ export const Dashboard = () => {
           )}
         </>
       ) : (
-        <div className={classes.welcome}>
-          <span className={`${classes.titleContainer} ${classes.titleBig}`}>
+        <div
+          className="d-flex flex-column w-100 align-items-center"
+          style={{ height: "80px", justifyContent: "space-between" }}
+        >
+          <span
+            className="text-white"
+            style={{
+              fontFamily: "League+Spartan",
+              fontWeight: 900,
+              fontSize: "24px",
+            }}
+          >
             Welcome to LiftLedger!
           </span>
-          <div className={classes.actionButtonContainer}>
+          <div style={{ width: "80px", height: "35px" }}>
             <div
-              className={`${classes.actionButton} ${classes.accountButtonBottom}`}
+              className="w-100 border border-0 text-white"
+              style={{
+                height: "30px",
+                fontFamily: "League+Spartan",
+                fontSize: "16px",
+                fontWeight: 600,
+                whiteSpace: "nowrap",
+                borderRadius: "5px",
+                background: "#004c81",
+                transform: "translateY(5px)",
+              }}
             />
             <button
-              className={`${classes.actionButton} ${classes.accountButtonTop}`}
+              className="w-100 border border-0 text-white"
+              style={{
+                height: "30px",
+                fontFamily: "League+Spartan",
+                fontSize: "16px",
+                fontWeight: 600,
+                whiteSpace: "nowrap",
+                borderRadius: "5px",
+                background: "#0096FF",
+                transform: "translateY(-30px)",
+                transition: "transform 0.1s",
+                cursor: "pointer",
+              }}
               onClick={() => {
                 toggleScreenState("fetching", true);
                 router.push("/auth/login");
